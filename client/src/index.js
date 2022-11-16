1807a8829fcfedcf1128def59a672a0d77586ec4
Selvadon123 <selvasuki1234@gmail.com>
2022-11-16 19:55:59 +0530

    first commit

---
+++ client/src/index.js
@@ -0,0 +1,5 @@
+// this is the file where we are going to connect with index html file 
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './App';
+ReactDom.render(<App />,document.getElementById('root'));
